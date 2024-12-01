ed82c0446fd6569d58c5599a3c442967b7def923
callmeartan <artanahmadi@icloud.com>
2024-12-02 02:50:41 +0300

    fixed clock showing

--- lib/widgets/interactive_time_picker.dart
+++ lib/widgets/interactive_time_picker.dart
@@ -20,7 +20,10 @@ class _InteractiveTimePickerState extends State<InteractiveTimePicker> {
   late int _selectedHour;
   late int _selectedMinute;
   late bool _isAM;
-  bool _showHours = true;
+  late TextEditingController _hourController;
+  late TextEditingController _minuteController;
+  late FocusNode _hourFocus;
+  late FocusNode _minuteFocus;
 
   @override
   void initState() {
@@ -29,26 +32,45 @@ class _InteractiveTimePickerState extends State<InteractiveTimePicker> {
     _selectedHour = now.hour % 12 == 0 ? 12 : now.hour % 12;
     _selectedMinute = now.minute;
     _isAM = now.hour < 12;
+    
+    _hourController = TextEditingController(text: _selectedHour.toString().padLeft(2, '0'));
+    _minuteController = TextEditingController(text: _selectedMinute.toString().padLeft(2, '0'));
+    _hourFocus = FocusNode();
+    _minuteFocus = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _hourController.dispose();
+    _minuteController.dispose();
+    _hourFocus.dispose();
+    _minuteFocus.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
+    final viewInsets = MediaQuery.of(context).viewInsets;
 
-    return Container(
-      decoration: BoxDecoration(
-        color: colorScheme.surface,
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _buildHeader(colorScheme),
-            _buildTimeDisplay(colorScheme),
-            _buildClock(colorScheme),
-            _buildActionButtons(colorScheme),
-          ],
+    return Padding(
+      padding: EdgeInsets.only(bottom: viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: colorScheme.surface,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _buildHeader(colorScheme),
+                _buildTimeDisplay(colorScheme),
+                _buildActionButtons(colorScheme),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -83,50 +105,101 @@ class _InteractiveTimePickerState extends State<InteractiveTimePicker> {
 
   Widget _buildTimeDisplay(ColorScheme colorScheme) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildTimeUnit(_selectedHour.toString().padLeft(2, '0'), _showHours, () {
-            setState(() => _showHours = true);
-          }, colorScheme),
-          Text(
-            ':',
-            style: GoogleFonts.poppins(
-              fontSize: 48,
-              fontWeight: FontWeight.w300,
-              color: colorScheme.onSurface,
+          _buildTimeTextField(
+            controller: _hourController,
+            focusNode: _hourFocus,
+            nextFocus: _minuteFocus,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                final hour = int.parse(value);
+                if (hour >= 1 && hour <= 12) {
+                  _selectedHour = hour;
+                } else {
+                  _hourController.text = _selectedHour.toString().padLeft(2, '0');
+                }
+                if (value.length == 2) _minuteFocus.requestFocus();
+              }
+            },
+            colorScheme: colorScheme,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Text(
+              ':',
+              style: GoogleFonts.poppins(
+                fontSize: 48,
+                fontWeight: FontWeight.w300,
+                color: colorScheme.onSurface,
+              ),
             ),
           ),
-          _buildTimeUnit(_selectedMinute.toString().padLeft(2, '0'), !_showHours, () {
-            setState(() => _showHours = false);
-          }, colorScheme),
-          const SizedBox(width: 20),
+          _buildTimeTextField(
+            controller: _minuteController,
+            focusNode: _minuteFocus,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                final minute = int.parse(value);
+                if (minute >= 0 && minute <= 59) {
+                  _selectedMinute = minute;
+                } else {
+                  _minuteController.text = _selectedMinute.toString().padLeft(2, '0');
+                }
+              }
+            },
+            colorScheme: colorScheme,
+          ),
+          const SizedBox(width: 12),
           _buildAmPmSelector(colorScheme),
         ],
       ),
     );
   }
 
-  Widget _buildTimeUnit(String value, bool isSelected, VoidCallback onTap, ColorScheme colorScheme) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: 80,
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: isSelected ? colorScheme.primary.withOpacity(0.1) : Colors.transparent,
-        ),
-        child: Text(
-          value,
-          textAlign: TextAlign.center,
-          style: GoogleFonts.poppins(
-            fontSize: 48,
-            fontWeight: FontWeight.w300,
-            color: isSelected ? colorScheme.primary : colorScheme.onSurface,
+  Widget _buildTimeTextField({
+    required TextEditingController controller,
+    required FocusNode focusNode,
+    FocusNode? nextFocus,
+    required Function(String) onChanged,
+    required ColorScheme colorScheme,
+  }) {
+    return SizedBox(
+      width: 80,
+      child: TextField(
+        controller: controller,
+        focusNode: focusNode,
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        maxLength: 2,
+        decoration: InputDecoration(
+          counterText: '',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: colorScheme.outline),
           ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: colorScheme.primary),
+          ),
+          contentPadding: const EdgeInsets.symmetric(vertical: 8),
         ),
+        style: GoogleFonts.poppins(
+          fontSize: 48,
+          fontWeight: FontWeight.w300,
+          color: colorScheme.onSurface,
+        ),
+        onChanged: (value) {
+          if (value.length > 2) {
+            controller.text = value.substring(0, 2);
+            controller.selection = TextSelection.fromPosition(
+              TextPosition(offset: controller.text.length),
+            );
+          }
+          onChanged(value);
+        },
       ),
     );
   }
@@ -162,152 +235,6 @@ class _InteractiveTimePickerState extends State<InteractiveTimePicker> {
               color: isSelected ? colorScheme.onPrimary : colorScheme.onSurface,
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildClock(ColorScheme colorScheme) {
-    return Container(
-      width: 280,
-      height: 280,
-      margin: const EdgeInsets.symmetric(vertical: 16),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          // Clock face
-          Container(
-            width: 280,
-            height: 280,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: colorScheme.onSurface.withOpacity(0.2),
-                width: 2,
-              ),
-            ),
-          ),
-          // Numbers
-          ..._buildClockNumbers(colorScheme),
-          // Clock hands
-          if (_showHours)
-            _buildHourHand(colorScheme)
-          else
-            _buildMinuteHand(colorScheme),
-          // Center dot
-          Container(
-            width: 8,
-            height: 8,
-            decoration: BoxDecoration(
-              color: colorScheme.primary,
-              shape: BoxShape.circle,
-            ),
-          ),
-          // Minute dot (only show when in minute mode)
-          if (!_showHours)
-            _buildMinuteDot(colorScheme),
-        ],
-      ),
-    );
-  }
-
-  List<Widget> _buildClockNumbers(ColorScheme colorScheme) {
-    List<Widget> numbers = [];
-    final radius = 120.0;
-
-    for (int i = 1; i <= 12; i++) {
-      final angle = (i * 30 - 90) * (pi / 180);
-      final x = cos(angle) * radius;
-      final y = sin(angle) * radius;
-
-      numbers.add(
-        Positioned(
-          left: 140 + x - 20,
-          top: 140 + y - 20,
-          child: _buildClockNumber(i, colorScheme),
-        ),
-      );
-    }
-
-    return numbers;
-  }
-
-  Widget _buildClockNumber(int number, ColorScheme colorScheme) {
-    final isSelected = _showHours
-        ? number == _selectedHour
-        : number * 5 == _selectedMinute;
-
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          if (_showHours) {
-            _selectedHour = number;
-          } else {
-            _selectedMinute = number * 5;
-          }
-        });
-      },
-      child: Container(
-        width: 40,
-        height: 40,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: isSelected ? colorScheme.primary : Colors.transparent,
-        ),
-        child: Center(
-          child: Text(
-            number.toString(),
-            style: GoogleFonts.poppins(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: isSelected ? colorScheme.onPrimary : colorScheme.onSurface,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildHourHand(ColorScheme colorScheme) {
-    final angle = (_selectedHour * 30 - 90) * (pi / 180);
-    return _buildHand(angle, 80, colorScheme);
-  }
-
-  Widget _buildMinuteHand(ColorScheme colorScheme) {
-    final angle = (_selectedMinute * 6 - 90) * (pi / 180);
-    return _buildHand(angle, 100, colorScheme);
-  }
-
-  Widget _buildHand(double angle, double length, ColorScheme colorScheme) {
-    return Transform.rotate(
-      angle: angle,
-      child: Container(
-        height: 2,
-        width: length,
-        decoration: BoxDecoration(
-          color: colorScheme.primary,
-          borderRadius: BorderRadius.circular(1),
-        ),
-        alignment: Alignment.centerLeft,
-      ),
-    );
-  }
-
-  Widget _buildMinuteDot(ColorScheme colorScheme) {
-    final angle = (_selectedMinute * 6 - 90) * (pi / 180);
-    final radius = 100.0;
-    final x = cos(angle) * radius;
-    final y = sin(angle) * radius;
-
-    return Positioned(
-      left: 140 + x - 6,
-      top: 140 + y - 6,
-      child: Container(
-        width: 12,
-        height: 12,
-        decoration: BoxDecoration(
-          color: colorScheme.primary,
-          shape: BoxShape.circle,
         ),
       ),
     );
